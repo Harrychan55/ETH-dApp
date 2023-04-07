e9c064638184381b7abce35dc3e41bd77ef8dd8a
Harrychan55 <s.doke.27639@gmail.com>
2023-04-07 12:50:20 +0900

    run.jsの理解UP　simplecode  try-catchのほうがエラー発生時わかりやすい。

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -16,14 +16,18 @@ const main = async () => {
     console.log("WavePortal address: ", wavePortal.address);
   };
   
-  const runMain = async () => {
-    try {
+    //runMain 関数が定義されます。この関数は、 main 関数を呼び出し、その結果を待機します。
+    //main 関数の処理が正常に完了した場合、プログラムは 0 のステータスコードで終了します。
+    //main 関数の処理中にエラーが発生した場合、エラーメッセージがログに出力され、プログラムは 1 のステータスコードで終了します。
+    //runMain 関数が呼び出され、プログラムが実行されます。
+    const runMain = async () => {
       await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
+       process.exit(0);
+    };
+
+  //const runMainを実行する。errorの場合logにerrorと表示し異常終了する。
   runMain();
+  runMain().catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
